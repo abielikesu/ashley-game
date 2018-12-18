e528d8f055ed2d2e11da5f90f3511160042724db
Abidis Solano <abielikesu@yahoo.com>
2018-12-18 22:03:29 +0000

    Added basic components and initial page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StartComponent} from './start/start.component';
+import {PlayComponent} from './play/play.component';
+import {ScoreComponent} from './score/score.component';
+import {LeaderboardComponent} from './leaderboard/leaderboard.component';
+import {SelectplayerComponent} from './selectplayer/selectplayer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'start', component: StartComponent },
+  { path: 'play', component: PlayComponent },
+  { path: 'score', component: ScoreComponent },
+  { path: 'leaderboard', component: LeaderboardComponent },
+  { path: 'selectplayer', component: SelectplayerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { PlayComponent } from './play/play.component';
+import { ScoreComponent } from './score/score.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { SelectplayerComponent } from './selectplayer/selectplayer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    PlayComponent,
+    ScoreComponent,
+    LeaderboardComponent,
+    SelectplayerComponent
   ],
   imports: [
     BrowserModule,
